0a87db9b4c20ef3e7a5d151868c099ccf040f7e4
RobPruzan <rapruzan@buffalo.edu>
2022-12-25 04:09:06 -0500

    Initial UI/ design

--- web/src/components/Analysis/Analysis.tsx
+++ web/src/components/Analysis/Analysis.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import ButtonAppBar from '../Navabars/MainNavbar';
 
 export const Analysis = () => {
-  return <div>Analysis</div>;
+  return (
+    <>
+      <ButtonAppBar />
+      <div className="angry-grid">
+        <div id="item-0">&nbsp;</div>
+        <div id="item-1">&nbsp;</div>
+        <div id="item-2">&nbsp;</div>
+        <div id="item-3">&nbsp;</div>
+        <div id="item-4">&nbsp;</div>
+      </div>
+    </>
+  );
 };
