c4b3ef8f4e235d81a0d5790a468e194b59e3808c
RobPruzan <rapruzan@buffalo.edu>
2023-03-11 12:58:20 -0500

    Setup collections and documents

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Collection, Excerpt, ExcerptInfo
+from .models import Collection, Excerpt, ExcerptInfo, MedicalDocument
 
 admin.site.register(Excerpt)
 admin.site.register(ExcerptInfo)
 admin.site.register(Collection)
+admin.site.register(MedicalDocument)
